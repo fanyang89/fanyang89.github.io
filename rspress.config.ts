7161f969d2a9d841a6cd04a953e06f6a76eb1da1
fanyang89 <fanyang89@outlook.com>
2024-03-16 23:44:16 +0800

    Support formula

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,11 +1,29 @@
 import * as path from "path";
 import { defineConfig } from "rspress/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   root: path.join(__dirname, "docs"),
   title: "Constrain Constant",
   description: "",
+  globalStyles: path.join(__dirname, "styles/index.css"),
+  markdown: {
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
+    showLineNumbers: true,
+    defaultWrapCode: true,
+    mdxRs: {
+      include: (p) => {
+        return !p.includes("pangu.mdx") && !p.includes("orthus.mdx");
+      },
+    },
+  },
+  mediumZoom: {
+    selector: ".rspress-doc img",
+  },
   themeConfig: {
+    enableContentAnimation: true,
     socialLinks: [
       {
         icon: "github",
